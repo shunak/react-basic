31bf658578804aff41a01822079700b76b7e7c8b
shunak <shuna966@gmail.com>
2021-01-14 18:24:08 +0900

    under condition rendering

--- src/components/BasicuseEffect.js
+++ src/components/BasicuseEffect.js
@@ -1,18 +1,25 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
 const BasicuseEffect = () => {
+  const [count, setCount] = useState(0); // 0 is initial value
+  const [item, setItem] = useState(""); // create another state
 
-    const [count, setCount] = useState(0)
+  useEffect(() => {
+    console.log("useEffect nvoked");
+  }, [count]);
 
-    useEffect(() => {
-        console.log("useEffect nvoked")
-    },[])
+  return (
+    <div>
+      <button onClick={() => setCount((precCount) => precCount + 1)}>
+        Click {count}
+      </button>
+      <input
+        type="text"
+        value={item}
+        onChange={(evt) => setItem(evt.target.value)}
+      />
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <button onClick={() => setCount(precCount => precCount + 1)}>Click { count}</button>
-        </div>  
-    )
-}
-
-export default BasicuseEffect
+export default BasicuseEffect;
