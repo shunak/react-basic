fa1e49afb33dee0c752a3b8766b5d7125fe86dae
shunak <shuna966@gmail.com>
2021-01-15 15:06:04 +0900

    api data get by id

--- src/components/ApiFetch.js
+++ src/components/ApiFetch.js
@@ -4,26 +4,43 @@ import axios from "axios";
 const ApiFetch = () => {
   const [posts, setPosts] = useState([]);
 
+  const [id, setId] = useState(1);
+  const [clicked, setClicked] = useState(false);
+
+  const handlerClicked = () => {
+    setClicked(!clicked);
+  };
+
   useEffect(() => {
     // axios.get("https://jsonplaceholder.typicode.com/posts")
     //     .then(res => {
     //         setPosts(res.data)
     //     })
 
-    fetch("https://jsonplaceholder.typicode.com/posts", { method: "GET" })
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, { method: "GET" })
       .then((res) => res.json())
       .then((data) => {
         setPosts(data);
       });
-  }, []);
+  }, [clicked]);
 
   return (
     <div>
-      <ul>
+      <input
+        type="text"
+        value={id}
+        onChange={(evt) => setId(evt.target.value)}
+      />
+      <button type="button" onClick={handlerClicked}>
+        Get post
+      </button>
+      <br />
+      {posts.title}
+      {/* <ul>
         {posts.map((post) => (
           <li key={post.id}>{post.title}</li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 };
